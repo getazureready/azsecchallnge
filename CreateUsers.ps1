c68f85a7ae41637bd55da80cbd52970c88943b02
getazureready <48854086+getazureready@users.noreply.github.com>
2022-03-30 10:17:26 +0000

    Add files via upload

--- CreateUsers.ps1
+++ CreateUsers.ps1
@@ -1,5 +1,5 @@
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-$PasswordProfile.Password = "M1crosoft123"
+$PasswordProfile.Password = "Pa55w.rd12345"
 $PasswordProfile.ForceChangePasswordNextLogin = "False"
 $tenant = (Get-AzureADTenantDetail).verifiedDomains.name
 # $UPN = "admin@" + $tenant
@@ -8,6 +8,6 @@ $tenant = (Get-AzureADTenantDetail).verifiedDomains.name
 # if ($currentuser.startswith("admin@", 1)) { New-AzureADUser -DisplayName "Admin" -PasswordProfile $PasswordProfile -UserPrincipalName $UPN -AccountEnabled $true -MailNickName "admin" -UsageLocation "GB" }
 
 $UPN = "Isaiah.Langer@" + $tenant
-New-AzureADUser -DisplayName "Isaiah Langer" -PasswordProfile $PasswordProfile -UserPrincipalName $UPN -AccountEnabled $true -MailNickName "IsaiahL" -UsageLocation "GB"
+New-AzureADUser -DisplayName "Isaiah Langer" -PasswordProfile $PasswordProfile -UserPrincipalName $UPN -AccountEnabled $true -MailNickName "IsaiahL" -UsageLocation "US"
 $UPN = "Alex.Wilber@" + $tenant
-New-AzureADUser -DisplayName "Alex Wilber" -PasswordProfile $PasswordProfile -UserPrincipalName $UPN -AccountEnabled $true -MailNickName "AlexL" -UsageLocation "GB"
+New-AzureADUser -DisplayName "Alex Wilber" -PasswordProfile $PasswordProfile -UserPrincipalName $UPN -AccountEnabled $true -MailNickName "AlexL" -UsageLocation "US"
