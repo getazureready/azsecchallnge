d22e2ac9b1d3c5a68a97cea6a4bf35b61c40eb79
getazureready <48854086+getazureready@users.noreply.github.com>
2022-03-31 09:51:47 +0000

    Add files via upload

--- CreateLab.ps1
+++ CreateLab.ps1
@@ -1,17 +1,34 @@
-$rg = (new-azurermresourcegroup -name Contoso-IaaS -Location eastus).ResourceGroupName
-$rg2 = (new-azurermresourcegroup -name Contoso-PaaS -Location eastus).ResourceGroupName
-$outputs = (new-azurermresourcegroupdeployment -Name azSecChLab -ResourceGroupName $rg -TemplateUri https://raw.githubusercontent.com/getazureready/azsecchallnge/master/azuredeploy/azuredeploy.json).Outputs
+﻿Write-Output "Enter the admin credentials for your tenant:"
+$creds=Get-Credential 
 
-$DestStorageAccount = $outputs.storageAccountName.Value
-$SourceStorageAccount = "azsecchlab"
-$destStorageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $rg -accountName $DestStorageAccount).value[0]
+sleep 3
+
+   Connect-AzAccount -Credential $subcredential
+   #Select-AzSubscription -Subscription "Azure Pass - Sponsorship"
+
+   $rg = (new-azresourcegroup -name Contoso-IaaS -Location eastus).ResourceGroupName
+   $rg2 = (new-azresourcegroup -name Contoso-PaaS2 -Location eastus).ResourceGroupName
+
+   sleep 10
+
+ New-AzResourceGroupDeployment -Name azSecChLab -ResourceGroupName $rg -TemplateUri https://raw.githubusercontent.com/getazureready/azsecchallnge/master/azuredeploy/azuredeploy.json
+ #above command will take 8-10 mins and will throw DSC extension errors on VM1 and VM2
+
+ sleep 10
+
+## Old-command## $DestStorageAccount = $outputs.storageAccountName.Value
+$DestStorageAccount = (Get-AzStorageAccount -ResourceGroupName $rg).StorageAccountName
+$SourceStorageAccount = "infraseclab"
+$destStorageKey = (Get-AzStorageAccountKey -ResourceGroupName $rg -accountName $DestStorageAccount).value[0]
 $sasToken = "?sv=2017-04-17&sr=c&si=infraseclab&sig=ONnlr56sGeZt0Qekgs8NFHquG5gGZU2jaFRnKp4bdXM%3D"
-$SourceStorageContext = New-AzureStorageContext $StorageAccountName $SourceStorageAccount -SasToken $sasToken
-$DestStorageContext = New-AzureStorageContext $StorageAccountName $DestStorageAccount -StorageAccountKey $DestStorageKey
-$SourceStorageContainer = 'azsecchlab'
-$DestStorageContainer = (new-azurestoragecontainer -Name contoso -permission Container -context $DestStorageContext).name
+$SourceStorageContext = New-AzStorageContext -StorageAccountName $SourceStorageAccount -SasToken $sasToken
+$DestStorageContext = New-AzStorageContext -StorageAccountName $DestStorageAccount -StorageAccountKey $DestStorageKey
+$SourceStorageContainer = 'infraseclab'
+$DestStorageContainer = (new-azstoragecontainer -Name contoso -permission Container -context $DestStorageContext).name
 
-$Blobs = (Get-AzureStorageBlob -Context $SourceStorageContext -Container $SourceStorageContainer)
+ sleep 5
+
+$Blobs = (Get-AzStorageBlob -Context $SourceStorageContext -Container $SourceStorageContainer)
 foreach ($Blob in $Blobs)
 {
    Write-Output "Moving $Blob.Name"
@@ -20,5 +37,7 @@ foreach ($Blob in $Blobs)
 }
 
 Write-Output "***** IaaS Lab Ready :-) *****"
-new-azurermresourcegroupdeployment -Name azSecChpaasLab -ResourceGroupName $rg2 -TemplateUri https://raw.githubusercontent.com/getazureready/azsecchallnge/master/azuredeploy/azuredeploy-paas.json
+
+new-azresourcegroupdeployment -Name azSecChpaasLab -ResourceGroupName $rg2 -TemplateUri https://raw.githubusercontent.com/getazureready/azsecchallnge/master/azuredeploy/azuredeploy-paas.json
+
 Write-Output "***** Azure Security Challenge Lab Ready :-) *****"
